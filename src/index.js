615840fd02e277873ce4c88850dac79b8fd82e7c
Kyle <kyle@kylemacquarrie.co.uk>
2020-06-29 22:31:07 +0100

    Tidy up render process - Don't begin rendering until all workers ready - Log progress on an interval, not every pixel

--- src/index.js
+++ src/index.js
@@ -8,18 +8,20 @@ const {
 const { writePpmFile } = require('./output')
 
 function main() {
-  const imageWidth = 384
+  const imageWidth = 1280
   const aspectRatio = 16/9
   const imageHeight = parseInt(imageWidth / aspectRatio, 10)
-  const samplesPerPixel = 100
+  const samplesPerPixel = 500
   const maxDepth = 50
-  const threads = os.cpus().length - 2
+  const threads = os.cpus().length - 1
   const started = Date.now()
   let progressPercent = 0
   const pixelArray = buildPixelArray(imageWidth, imageHeight)
   const showProgress = true
   const totalPixels = (imageHeight * imageWidth)
   let currentPixel = 0
+  const progressUpdateFrequency = 1000
+  let progressUpdateInterval = null
 
   const workerData = {
     samplesPerPixel,
@@ -33,16 +35,10 @@ function main() {
     pixelArray[data.y][data.x] = new Color(data.r, data.g, data.b)
 
     currentPixel ++
-    if (showProgress) {
-      const currentProgressPercent = parseInt((currentPixel / totalPixels) * 100, 10)
-      if (currentProgressPercent > progressPercent) {
-        progressPercent = currentProgressPercent
-        process.stdout.write(`\r${progressBar({ progress: progressPercent })}`)
-      }
-    }
 
     if (currentPixel === totalPixels) {
       workers.forEach(w => w.terminate())
+      clearInterval(progressUpdateInterval)
       if (showProgress) process.stdout.write(`\r\n`)
       const totalTime = (Date.now() - started) / 1000
       console.log(`Rendered ${totalPixels.toLocaleString('en-GB')} pixels in ${totalTime} seconds`)
@@ -51,23 +47,46 @@ function main() {
   }
 
   console.log(`Initialising ${threads} workers`)
+
   const workers = Array.from({ length: threads }, () => new Worker('./src/Renderer/renderWorker.js', { workerData }))
+
+  if (showProgress) {
+    progressUpdateInterval = setInterval(() => {
+      const currentProgressPercent = parseInt((currentPixel / totalPixels) * 100, 10)
+      if (currentProgressPercent > progressPercent) {
+        progressPercent = currentProgressPercent
+        process.stdout.write(`\r${progressBar({ progress: progressPercent })}`)
+      }
+    }, progressUpdateFrequency)
+  }
+
+  let workersOnline = 0
 
   workers.forEach(w => {
     const id = workers.indexOf(w) + 1
-    w.once('online', () => { console.log(`worker ${id} online`)})
+
+    w.once('online', () => {
+      console.log(`worker ${id} online`)
+      workersOnline ++
+      if(workersOnline === threads) render()
+    })
 
     w.on('message', handleMessage)
+
+    w.on('error', err => { console.log(`worker ${id} encountered an error: ${err}`)})
 
     w.once('exit', exitcode => { console.log(`worker ${id} exited with exit code ${exitcode}`)})
   })
 
-  for (let j = 0; j < imageHeight; j++ ) {
-    for (let i = 0; i < imageWidth; i++) {
-      workers[i % threads].postMessage({
-        x: i,
-        y: j
-      })
+  function render() {
+    console.log('beginning render...')
+    for (let j = 0; j < imageHeight; j++ ) {
+      for (let i = 0; i < imageWidth; i++) {
+        workers[i % threads].postMessage({
+          x: i,
+          y: j
+        })
+      }
     }
   }
 }
